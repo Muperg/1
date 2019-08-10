6bead19e3f3cba922123e25cfcb4cd4ed84edb89
Muperg <53973022+Muperg@users.noreply.github.com>
2019-08-10 16:49:05 +0300

    Update script.js

--- activebox/script.js
+++ activebox/script.js
@@ -1,12 +1,1 @@
-$(function(){
 
-    $('.menuToggle').on('click', function() {
-       $('.menu').slideToggle(300, function(){
-            if( $(this).css('display') === "none"){
-                $(this).removeAttr('style');
-            }
-       });
-
-    });
-
-});
